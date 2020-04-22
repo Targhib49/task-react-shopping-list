15f4041bd6cd6e4e2527d7f6aee3c050614cb5ca
Targhib49 <targhib.ibrahim@gmail.com>
2020-04-22 12:11:30 +0700

    add logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import { MDBNavbar, MDBNavbarBrand } from 'mdbreact';
+import { MDBNavbar, MDBNavbarBrand, MDBBtn } from 'mdbreact';
+import { withRouter } from 'react-router-dom';
 
 class NavbarPage extends Component {
-	state = {
-		isOpen: false
-	};
-
-	toggleCollapse = () => {
-		this.setState({ isOpen: !this.state.isOpen });
+	logoutCheck = () => {
+		localStorage.setItem('isLogin', false);
+		this.props.history.push('/');
 	};
 
 	render() {
 		return (
-			<MDBNavbar color="indigo" dark expand="md">
+			<MDBNavbar color="indigo" dark expand="md" className="justify-content-between">
 				<MDBNavbarBrand>
 					<strong className="white-text">Kulakan Shopping Center</strong>
 				</MDBNavbarBrand>
+				<MDBBtn className="logout" onClick={this.logoutCheck}>
+					Logout
+				</MDBBtn>
 			</MDBNavbar>
 		);
 	}
 }
 
-export default NavbarPage;
+export default withRouter(NavbarPage);
